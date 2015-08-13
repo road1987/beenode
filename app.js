33660f281556496cd8d5930dfc94bd1cc27337dc
hongchun.li <bee0873@163.com>
2015-08-13 20:01:42 +0800

    Modify app server start from app.js for the use of nodemon

--- app.js
+++ app.js
@@ -58,4 +58,12 @@ app.use(function(err, req, res, next) {
 });
 
 
-module.exports = app;
+//module.exports = app;
+//for develop 
+var debug = require('debug')('express_demo');
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  debug('Express server listening on port ' + server.address().port);
+});
